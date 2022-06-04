b8d536cab623aa9f27b90396fb271a248ca3dc1e
Aadhishankaran <78032579+Aadhishankaran@users.noreply.github.com>
2022-06-04 10:31:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function setup() {
      canvas = createCanvas(600, 500);
@@ -6,6 +10,9 @@ function setup() {
 
      video = createCapture(VIDEO);
      video.hide();
+
+     poseNet = ml5.poseNet(video,modelLoaded)
+     poseNet.on('pose' , gotPoses);
 }
 
 function draw () {
@@ -15,9 +22,38 @@ function draw () {
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+    document.getElementById("play_button").disabled = true;
+}
+
+function stop()
+{
+    song.stop()
+    document.getElementById("play_button").disabled = false;
 }
 
 function preload()
 {
     song = loadSound("music.mp3")
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized')
+}
+
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results)
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY = "+ rightWristY);
+    }
 }
